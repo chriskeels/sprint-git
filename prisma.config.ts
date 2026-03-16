714853bcacc3b0a08bef1e5bb1f3554a961be802
User <user@example.com>
2026-03-16 15:57:52 -0400

    fix: make prisma generate work in CI

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,11 @@
-export default {
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: env("DATABASE_URL"),
   },
-};
+});
